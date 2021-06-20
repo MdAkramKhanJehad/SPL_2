4336b3330f1dce1c5a66c2a36ad7b50fe5ba1edc
Ashrafulgafurtantan <bsse1021@iit.du.ac.bd>
2021-06-20 17:56:40 +0600

    Shared Functions added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-final FirebaseFirestore  _firestore = FirebaseFirestore.instance;
+import 'shared/shared_objects.dart';
+import 'shared/shared_functions.dart';
+final SharedFunctions sharedFunctionsGlobal = new SharedFunctions();
+final SharedObjects sharedObjectsGlobal = new SharedObjects();
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -9,102 +11,84 @@ void main() async{
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        fontFamily: 'Mina',
-        primarySwatch: Colors.blue,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return LayoutBuilder(
+        builder: (context, constraints) {
+          sharedFunctionsGlobal.getMobileHeightWeight(constraints);
+          return MaterialApp(
+            title: 'Agri Pro',
+            theme: ThemeData(
+              fontFamily: 'Mina',
+              primarySwatch: Colors.blue,
+            ),
+            debugShowCheckedModeBanner: false,
+            home: MyHomePage(),
+          );
+        }
     );
   }
 }
-
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-
-  final String title;
+  const MyHomePage({Key? key, }) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-  test()async{
-    _firestore.collection('test').add({
-      "name":"Ashraful Gafur",
-      'age': 23,
-    }).then((dofRef) =>{
-     print("Id is = ${dofRef.id}")
-    });
-  }
-  @override
-  void initState() {
-    test();
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    return Scaffold(
+      body: Stack(
+        children: [
+          sharedFunctionsGlobal.getMainPageBackgroundImage(),
+          Center(
+            child: GestureDetector(
+              onTap: (){
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context)=>AnotherClass()));
+              },
+              child: Container(
+                child: Text("Hello"),
+              ),
+            ),
+          ),
+        ],
+      )
+    );
+  }
+}
+
+class AnotherClass extends StatefulWidget {
+  const AnotherClass({Key? key}) : super(key: key);
+
+  @override
+  _AnotherClassState createState() => _AnotherClassState();
+}
+
+class _AnotherClassState extends State<AnotherClass> {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Another Class"),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: Container(
+          child: Text("Another Class"),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+
+class SizeConfig{
+  void init(BoxConstraints constraints ){
+    print(constraints.maxHeight);
+    print(constraints.maxWidth);
+    print("------------------------------");
+
   }
 }
