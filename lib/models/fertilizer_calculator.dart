201ef72d2999436e7ccdeccf9262ebee0cae9853
Ashrafulgafurtantan <bsse1021@iit.du.ac.bd>
2021-08-21 17:09:40 +0600

    My changes

--- lib/models/fertilizer_calculator.dart
+++ lib/models/fertilizer_calculator.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-
 class FertilizerCalculator{
   final List<String> units;
   final String name;
--- lib/navbar_items/weather_page/weather_page.dart
+++ lib/navbar_items/weather_page/weather_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:geolocator/geolocator.dart';
-import 'package:intl/intl.dart';
 import 'package:weather/weather.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
@@ -18,7 +17,7 @@ class WeatherForecast extends StatefulWidget {
 class _WeatherForecastState extends State<WeatherForecast> {
 
   String key =  dotenv.env['WEATHER_KEY'].toString();
-  String _indicator = "Search by Location";
+  String _indicator = "Press the button";
   late WeatherFactory ws;
   List<Weather> _data = [];
   AppState _state = AppState.NOT_DOWNLOADED;
@@ -61,6 +60,7 @@ class _WeatherForecastState extends State<WeatherForecast> {
   }
 
   void queryWeather(bool isCity,String city) async {
+    // Removes keyboard
     FocusScope.of(context).requestFocus(FocusNode());
 
     setState(() {
@@ -95,23 +95,19 @@ class _WeatherForecastState extends State<WeatherForecast> {
       ListView.separated(
         itemCount: _data.length,
         itemBuilder: (context, index) {
-          return Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              color: Colors.white70,
-              child:
-              ListTile(
-                title: Text("Location: ${_data[index].areaName.toString()}, ${_data[index].country.toString()}"),
+          return ListTile(
+            title: Text("${_data[index].areaName.toString()}, ${_data[index].country.toString()} || "
+                "Humidity: ${_data[index].humidity.toString()}",style: TextStyle(fontSize: 14),),
+            subtitle: Text("T:${_data[index].temperature.toString().split(" ")[0]}\u2103 || "
+                "Feels like:${_data[index].tempFeelsLike.toString().split(" ")[0]}\u2103\n"
+                "Max: ${_data[index].tempMax.toString().split(" ")[0]}\u2103 || "
+                "Min: ${_data[index].tempMin.toString().split(" ")[0]}\u2103"),
+            leading:
+            Text("${_data[index].date.toString().split(" ")[0]},\n"
+                "${(_data[index].date.toString().split(" ")[1]).split(".000")[0]}",style: TextStyle(fontSize: 13),)
+            ,
+            trailing:Text("${_data[index].weatherMain},\n${_data[index].weatherDescription}",style: TextStyle(fontSize: 14),),
 
-                subtitle: Text("T: ${_data[index].temperature.toString().split(" ")[0]}\u2103 || "
-                    "Feels like: ${_data[index].tempFeelsLike.toString().split(" ")[0]}\u2103\n"
-                    "Max: ${_data[index].tempMax.toString().split(" ")[0]}\u2103 || "
-                    "Min: ${_data[index].tempMin.toString().split(" ")[0]}\u2103"),
-
-                trailing:Text("Time: ${_data[index].date.toString().split(" ")[0]},${(DateFormat.jm().format(_data[index].date!))}\nWeather: ${_data[index].weatherDescription}",style: TextStyle(fontSize: 15),),
-
-              )
           );
         },
         separatorBuilder: (context, index) {
@@ -148,9 +144,7 @@ class _WeatherForecastState extends State<WeatherForecast> {
             Text(
               indicator,
               style: TextStyle(
-                  fontSize: 23,
-                  fontFamily: "Mina",
-                  fontWeight: FontWeight.bold
+                fontSize: 23,
               ),
             ),
           ],
@@ -179,51 +173,20 @@ class _WeatherForecastState extends State<WeatherForecast> {
     print(position.latitude.toString());
   }
 
-  IconButton IconMaker(BuildContext context,IconData icon,double sizee){
-    return IconButton(
-      color: Colors.black,
-      icon: Icon(icon, size: sizee,),
-      onPressed: (){
-        setState(() {
-          final FocusScopeNode currentScope = FocusScope.of(context);
-          if (!currentScope.hasPrimaryFocus && currentScope.hasFocus) {
-            FocusManager.instance.primaryFocus!.unfocus();
-          }
-          myController.clear();
-
-        });
-      },
-    );
-  }
 
   Widget _inputCityName() {
     return Row(
       children: <Widget>[
+
         Expanded(
             child: Container(
                 margin: EdgeInsets.all(5),
-                child: TextFormField(
-                  autofocus: false,
+                child: TextField(
                   controller: myController,
                   decoration: InputDecoration(
-                    prefixIcon: IconButton(
-                      icon: Icon(Icons.search,size: 30,color: Colors.teal.shade500,),
-                      onPressed: () {
-                        final FocusScopeNode currentScope = FocusScope.of(context);
-                        if (!currentScope.hasPrimaryFocus && currentScope.hasFocus) {
-                          FocusManager.instance.primaryFocus!.unfocus();
-                        }
-                      },
-                    ),
-                    contentPadding: EdgeInsets.only(left: 10, top: 14,bottom: 15),
-                    hintText: 'Enter City (Default:Current Location)',
-                    hintStyle: TextStyle(fontFamily: 'Mina',fontSize: 14),
-                    border: OutlineInputBorder(),
-                    suffixIcon: IconMaker(context, Icons.clear,25),
-                  ),
-                )
-            )
-        )
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter City (Default:Current Location)'),
+                )))
       ],
     );
   }
@@ -234,36 +197,38 @@ class _WeatherForecastState extends State<WeatherForecast> {
       children: <Widget>[
         Container(
           margin: EdgeInsets.all(5),
-          child:
-          ElevatedButton(
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.teal.shade400)
-              ),
-              child: Text('Fetch Weather', style: TextStyle(fontWeight: FontWeight.bold, fontFamily: "Mina",fontSize: 15),),
-              onPressed: () {
-                if(myController.text.isEmpty){
-                  queryWeather(false,"");
-                }else{
-                  queryWeather(true,myController.text);
-                }
+          child: TextButton(
+            child: Text(
+              'Fetch weather',
+              style: TextStyle(color: Colors.white),
+            ),
+            onPressed: (){
+              if(myController.text.isEmpty){
+                queryWeather(false,"");
+              }else{
+                queryWeather(true,myController.text);
               }
+            },
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.blue)),
           ),
         ),
         Container(
           margin: EdgeInsets.all(5),
-          child:
-          ElevatedButton(
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.teal.shade400)
-              ),
-              child: Text('Fetch Forecast', style: TextStyle(fontWeight: FontWeight.bold, fontFamily: "Mina", fontSize: 15),),
-              onPressed: () {
-                if(myController.text.isEmpty){
-                  queryForecast(false,"");
-                }else{
-                  queryForecast(true,myController.text);
-                }
+          child: TextButton(
+            child: Text(
+              'Fetch forecast',
+              style: TextStyle(color: Colors.white),
+            ),
+            onPressed: (){
+              if(myController.text.isEmpty){
+                queryForecast(false,"");
+              }else{
+                queryForecast(true,myController.text);
               }
+            },
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.blue)),
           ),
         )
       ],
@@ -272,33 +237,20 @@ class _WeatherForecastState extends State<WeatherForecast> {
 
   @override
   Widget build(BuildContext context) {
-    final double height = MediaQuery.of(context).size.height;
-    final double width = MediaQuery.of(context).size.width;
-    double heightMultiplier = height/712;
-    double widthMultiplier= width/360;
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor:  Colors.white,
-          backwardsCompatibility: false,
-          automaticallyImplyLeading: false,
-          centerTitle: true,
-          elevation: 0,
-          leading: IconButton(icon: Icon(Icons.arrow_back_ios,color: Colors.teal.shade800,size: 18*widthMultiplier ,),
-            onPressed: (){
-              Navigator.pop(context);
-            },),
-          title: Text("Weather Forecast", style:TextStyle( fontFamily: "Mina", letterSpacing: 0, fontSize: 20*widthMultiplier,fontWeight: FontWeight.w800, color:Colors.teal.shade800,) ),
+          backgroundColor: Colors.white,
+          title: Center(child: Text('Weather',style: TextStyle(color: Colors.blueGrey,fontWeight: FontWeight.bold),)),
         ),
         body: Column(
           children: <Widget>[
             _inputCityName(),
             _buttons(),
             Text(
-              'Result:',
-              style: TextStyle(fontSize: 20, fontFamily: "Mina", fontWeight: FontWeight.bold),
+              'Output:',
+              style: TextStyle(fontSize: 20),
             ),
             Divider(
               height: 20.0,
